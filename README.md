4eb889d64abea3fb71edf0280bd7982213e0bf07
Jash-tech <jashsuke@gmail.com>
2024-08-31 16:57:32 +0530

    data ingestion

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # FinancialFraudDetection
+
+## Workflow
+constant
+config_entity
+artifact_entity
+conponent
+pipeline
+app.py / demo.py
---
+++ demo.py
@@ -0,0 +1,4 @@
+from financial_fraud_detection.pipeline.training_pipeline import TrainPipeline
+
+obj=TrainPipeline()
+obj.run_pipeline()
